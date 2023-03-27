e05242394b4454b6041c4e8fc789d890c78fada0
Rakesh Kumar <rkrakeshand7796@gmail..com>
2023-03-27 13:36:37 +0530

    Added Loop and Flexipage

---
+++ force-app/main/default/lwc/loopingOrIteration/loopingOrIteration.js
@@ -0,0 +1,8 @@
+import {
+    LightningElement,
+    track
+} from 'lwc';
+
+export default class LoopingOrIteration extends LightningElement {
+    @track cityList = ['Patna', 'Delhi', 'Bengaluru', 'Hyderabad', 'pune'];
+}
